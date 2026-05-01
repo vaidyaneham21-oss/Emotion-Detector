773fb616dccdc52581267e80912dd26f1e0dbd72
vaidyaneham21-oss <vaidyaneham21@gmail.com>
2026-05-01 20:01:20 +0530

    Create emotion_detection.py

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,9 @@
+def emotion_detector(text_to_analyze):
+    return {
+        "anger": 0.01,
+        "disgust": 0.01,
+        "fear": 0.01,
+        "joy": 0.96,
+        "sadness": 0.01,
+        "dominant_emotion": "joy"
+    }
